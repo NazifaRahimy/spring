c79d9b6d82f6efc7b1e3ee7e42ef4ca2888c3556
nazifaRahimi <nazifarahimy6@gmail.com>
2025-05-05 16:32:13 +0430

    add text to project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ export default function Home() {
   return (
     <div className="w-full h-[500px] border bg-blue-500 text-center">
          <h1 className="text-white text-3xl font-bold mt-[150px]">Hello word </h1>
+         <p className="mt-10 px-20 text-white text-left"
+         >Spring, also known as springtime, is one of the four temperate seasons, succeeding winter and preceding summer. There are various technical definitions of spring, but local usage of the term varies according to local climate, cultures and customs. When it is spring in the Northern Hemisphere, it is autumn in the Southern Hemisphere and vice versa. At the spring equinox, also called the vernal equinox, days and nights are approximately twelve hours long, with daytime length increasing and nighttime length decreasing as the season progresses until the summer solstice. The spring equinox is in March in the Northern Hemisphere and in September in the Southern Hemisphere, while the summer solstice is in June in the Northern Hemisphere and in December in the Southern Hemisphere.</p>
     </div>
   );
 }
