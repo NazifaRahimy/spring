e70ede3ff019f12277d5e72e3d23b80f693d419f
nazifaRahimi <nazifarahimy6@gmail.com>
2025-05-07 10:20:46 +0430

    style spring article section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,15 @@
 
 export default function Home() {
   return (
-    <div >
-        <div dir="rtl" >
-            <h1 ><span >روزانه</span>    » <span > متن و جمله</span></h1>
-            <h2 >متن عاشقانه فصل بهار + جملات زیبا در وصف بهار و فصل سرسبزی</h2>
-            <p >فصل بهار شروع زیبایی ها است و سرسبزی این فصل باعث نشاط و سرزندگی روحیه می شود. با فرا رسیدن فصل بهار خیلی از افراد خوشحال تر هستند زیرا رشد درختان و آواز پرندگان را می شنوند و هوا نیز بسیار لطیف است. در این فصل زیبا، گل ها به آرامی شکوفه می دهند و طبیعت بار دیگر نو می شود و لباس سبز به تن می کند. این فصل در روحیه انسان ها هم بسیار تاثیر می گذارد و حس عشق را در وجود انسان بیش از پیش می کند. به همین مناسبت زیباترین متن های عاشقانه ویژه فصل بهار را در ادامه گردآوری کرده ایم.</p>
+    <div  className="w-full  box-border px-[80px] py-10" >
+        <div dir="rtl" className="w-[900px] mx-auto ">
+        <h1 className="text-2xl mt-2 mb-4 font-semibold text-gray-500 "><span className=" text-[#232323] hover:text-blue-600 cursor-pointer ">روزانه</span>    » <span className=" text-[#232323] hover:text-blue-600 cursor-pointer "> متن و جمله</span></h1>
+            <h2 className="mt-5 mb-4 text-[#232323] font-semibold text-xl">متن عاشقانه فصل بهار + جملات زیبا در وصف بهار و فصل سرسبزی</h2>
+            <p className="font-normal text-lg">فصل بهار شروع زیبایی ها است و سرسبزی این فصل باعث نشاط و سرزندگی روحیه می شود. با فرا رسیدن فصل بهار خیلی از افراد خوشحال تر هستند زیرا رشد درختان و آواز پرندگان را می شنوند و هوا نیز بسیار لطیف است. در این فصل زیبا، گل ها به آرامی شکوفه می دهند و طبیعت بار دیگر نو می شود و لباس سبز به تن می کند. این فصل در روحیه انسان ها هم بسیار تاثیر می گذارد و حس عشق را در وجود انسان بیش از پیش می کند. به همین مناسبت زیباترین متن های عاشقانه ویژه فصل بهار را در ادامه گردآوری کرده ایم.</p>
         </div>
-        <div >
-            <div >
-                <img src="https://roozaneh.net/wp-content/uploads/2024/03/photo_2024-03-10_17-35-26.jpg.webp" alt="" />
+        <div className="w-full flex items-center justify-center  flex-col">
+            <div className="w-[900px] h-[450px] rounded-[20px]  mt-10">
+                <img className="w-[900px] h-[450px] rounded-[20px]" src="https://roozaneh.net/wp-content/uploads/2024/03/photo_2024-03-10_17-35-26.jpg.webp" alt="" />
             </div>
       </div>
     </div>
