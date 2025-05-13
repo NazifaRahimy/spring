9433983cab15cbbf0862f4559ecb0c1e3a4a590b
nazifaRahimi <nazifarahimy6@gmail.com>
2025-05-13 09:38:48 +0430

    Apply layout and visual styles: background color, centered text, padding, rounded corners, and spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -93,16 +93,16 @@ export default function Home() {
           <p className="text-xl font-normal px-3">روی گونه‌ های زنی که من باشم</p>
           <p  className="text-xl font-normal px-3">!شگوفه بدهد</p>
       </div>
-        <h1 >جملات ادبی در مورد بهار</h1>
-        <div  >
-          <div ><FaQuoteRight /></div>
-          <p >بـــهار را ببین</p>
-          <p >چگونه از خود می تــکانَد برگ های فرسوده را</p>
-          <p >تو هم جوانه ای بزن</p>
-          <p >ســبز شـــو</p>
-          <p >بـهـار</p>
-          <p >ادامه ی لبخندهای تو خواهد بود…</p>
-          <p >“مینا آقازاده”</p>
+        <h1 className="text-2xl mt-10 mb-4 font-semibold  text-center">جملات ادبی در مورد بهار</h1>
+        <div className="bg-gray-100 text-center w-[900px] h-auto   rounded-2xl border-box px-10 mx-auto mt-10 pb-5" >
+          <div className="  mx-auto  w-[150px] text-center bg-gray-100 shadow-blue-100 shadow-md rounded-br-full rounded-bl-full flex justify-center pb-4 pt-3  mb-5"><FaQuoteRight /></div>
+          <p className="text-xl font-normal px-3">بـــهار را ببین</p>
+          <p className="text-xl font-normal px-3">چگونه از خود می تــکانَد برگ های فرسوده را</p>
+          <p className="text-xl font-normal px-3">تو هم جوانه ای بزن</p>
+          <p className="text-xl font-normal px-3">ســبز شـــو</p>
+          <p className="text-xl font-normal px-3">بـهـار</p>
+          <p className="text-xl font-normal px-3">ادامه ی لبخندهای تو خواهد بود…</p>
+          <p className="text-xl font-normal px-3">“مینا آقازاده”</p>
       </div>
     </div>
 
