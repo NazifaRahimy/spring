13bef8a44d68635e90bdfd70a350936862649418
nazifaRahimi <nazifarahimy6@gmail.com>
2025-05-14 10:43:34 +0430

    Decorative quote icon container with shadow and rounded bottom edges

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -136,11 +136,11 @@ export default function Home() {
           <p className="text-xl font-normal px-3">با شکفتن اولین شکوفه در بهار بود که عشق به خانه من آمد</p>
           <p className="text-xl font-normal px-3">اینک در بهاری دیگر سرشار از حس زیستنم کنار تو همسر دوست داشتنی من</p>
       </div>
-        <div  >
-          <div ><FaQuoteRight /></div>
-          <p >بوی مطبوعِ گل و منظره‌ای رو به بهار</p>
-          <p >پنجره پشت ِ خودش یک من و تو کم دارد</p>
-          <p >علیرضا صادقی</p>
+        <div className="bg-gray-100 text-center w-[900px] h-auto   rounded-2xl border-box px-10 mx-auto mt-10 pb-5" >
+          <div className="  mx-auto  w-[150px] text-center bg-gray-100 shadow-blue-100 shadow-md rounded-br-full rounded-bl-full flex justify-center pb-4 pt-3  mb-5"><FaQuoteRight /></div>
+          <p className="text-xl font-normal px-3">بوی مطبوعِ گل و منظره‌ای رو به بهار</p>
+          <p className="text-xl font-normal px-3">پنجره پشت ِ خودش یک من و تو کم دارد</p>
+          <p className="text-xl font-normal px-3">علیرضا صادقی</p>
       </div>
     </div>
 
