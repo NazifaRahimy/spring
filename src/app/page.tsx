9bc9b594987a01da1deaafbd532ec40e2c5d876b
nazifaRahimi <nazifarahimy6@gmail.com>
2025-05-14 10:35:40 +0430

    Decorative quote icon centered at the top of the box

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -125,11 +125,11 @@ export default function Home() {
           <p className="text-xl font-normal px-3">.... بهار هم عاشق شد</p>
       </div>
       <h2 className="mt-10 text-center mb-4 text-[#232323] font-semibold text-xl">عکس نوشته عاشقانه بهاری</h2>
-      <div >
+      <div className="bg-gray-100 text-center w-[900px] h-auto   rounded-2xl border-box px-10 mx-auto mt-10 pb-5" >
            <div className="  mx-auto  w-[150px] text-center bg-gray-100 shadow-blue-100 shadow-md rounded-br-full rounded-bl-full flex justify-center pb-4 pt-3  mb-5"><FaQuoteRight /></div>
-           <p >زیبای من</p>
-           <p >به شوق تو قدم در خیابان می گذارم</p>
-           <p >درختان بهاری را تماشا می کنم و مست می شوم از بوی شکوفه و سبزه و <span className="text-blue-700 hover:text-red-600 duration-100">باران</span></p>
+           <p className="text-xl font-normal px-3">زیبای من</p>
+           <p className="text-xl font-normal px-3">به شوق تو قدم در خیابان می گذارم</p>
+           <p className="text-xl font-normal px-3">درختان بهاری را تماشا می کنم و مست می شوم از بوی شکوفه و سبزه و <span className="text-blue-700 hover:text-red-600 duration-100">باران</span></p>
       </div>
     </div>
 
