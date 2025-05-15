15f936f3649afe67329248ae64f307a6c1fe8f1f
nazifaRahimi <nazifarahimy6@gmail.com>
2025-05-15 09:06:12 +0430

    Displaying a spring-themed image inside a styled, centered container with rounded corners.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -165,6 +165,9 @@ export default function Home() {
           <p className="text-xl font-normal px-3">حتی اگر دلش؛</p>
           <p className="text-xl font-normal px-3">....جا مانده باشد میان برف‌ ها</p>
      </div>
+       <div className="w-[900px]   rounded-[20px]  mt-10 mx-auto">
+          <img className="w-[900px]  rounded-[20px]"src="https://roozaneh.net/wp-content/uploads/2024/03/bahare-ziba-1.jpg.webp" alt="" />
+     </div>
     </div>
 
   );
