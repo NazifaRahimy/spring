a8e9847fe6258100fac3608873b59a724a09a816
nazifaRahimi <nazifarahimy6@gmail.com>
2025-05-19 09:54:05 +0430

    Add spring image section to the layout

--- src/app/text/page.tsx
+++ src/app/text/page.tsx
@@ -9,12 +9,15 @@ export default function Text() {
                     <p className="text-xl font-normal px-3">هزار بار بیاید بهار، کافی نیست</p>
                     <p className="text-xl font-normal px-3">فاضل نظری</p>
                 </div>
-                 <div className="bg-gray-100 text-center w-[900px] h-auto  rounded-2xl border-box px-10 mx-auto mt-10 pb-5" >
-                       <div className="  mx-auto  w-[150px] text-center bg-gray-100 shadow-blue-100 shadow-md rounded-br-full rounded-bl-full flex justify-center pb-4 pt-3  mb-5"><FaQuoteRight /></div>
-                      <p className="text-xl font-normal px-3">نهال بودم و در حسرتِ بهار! ولی</p>
-                      <p className="text-xl font-normal px-3">درخت می‌شوم و شوق برگ و بارم نیست</p>
-                      <p className="text-xl font-normal px-3">فاضل نظری</p>
-                  </div>
+                <div className="bg-gray-100 text-center w-[900px] h-auto  rounded-2xl border-box px-10 mx-auto mt-10 pb-5" >
+                    <div className="  mx-auto  w-[150px] text-center bg-gray-100 shadow-blue-100 shadow-md rounded-br-full rounded-bl-full flex justify-center pb-4 pt-3  mb-5"><FaQuoteRight /></div>
+                    <p className="text-xl font-normal px-3">نهال بودم و در حسرتِ بهار! ولی</p>
+                    <p className="text-xl font-normal px-3">درخت می‌شوم و شوق برگ و بارم نیست</p>
+                    <p className="text-xl font-normal px-3">فاضل نظری</p>
+                </div>
+                <div >
+                    <img src="https://roozaneh.net/wp-content/uploads/2024/03/bahare-ziba-4.jpg.webp" alt="" />
+                </div>
         </div>
   )
 }
