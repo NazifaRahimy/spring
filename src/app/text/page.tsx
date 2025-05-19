8c69a2412757ee7f51f789fa2605daa426e38bf6
nazifaRahimi <nazifarahimy6@gmail.com>
2025-05-19 09:46:16 +0430

    Style romantic quote box with centered text, rounded corners, padding, and shadow

--- src/app/text/page.tsx
+++ src/app/text/page.tsx
@@ -1,13 +1,13 @@
 import { FaQuoteRight } from "react-icons/fa";
-export default function Home() {
+export default function Text() {
   return (
-          <div >
-                <h2 >متن عاشقانه فصل بهار</h2>
-                <div >
-                    <div ><FaQuoteRight /></div>
-                    <p >چنان که یخ زده تقویم‌ها اگر هر روز</p>
-                    <p >هزار بار بیاید بهار، کافی نیست</p>
-                    <p >فاضل نظری</p>
+          <div className="w-full  box-border px-[80px] py-10">
+                <h2 className="mt-10 text-center mb-4 text-[#232323] font-semibold text-xl">متن عاشقانه فصل بهار</h2>
+                <div className="bg-gray-100 text-center w-[900px] h-auto  rounded-2xl border-box px-10 mx-auto mt-10 pb-5" >
+                    <div className="  mx-auto  w-[150px] text-center bg-gray-100 shadow-blue-100 shadow-md rounded-br-full rounded-bl-full flex justify-center pb-4 pt-3  mb-5"><FaQuoteRight /></div>
+                    <p className="text-xl font-normal px-3">چنان که یخ زده تقویم‌ها اگر هر روز</p>
+                    <p className="text-xl font-normal px-3">هزار بار بیاید بهار، کافی نیست</p>
+                    <p className="text-xl font-normal px-3">فاضل نظری</p>
                 </div>
         </div>
   )
