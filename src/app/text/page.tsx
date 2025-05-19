a875589653d40cb33d690ce09b8fb602351ea28a
nazifaRahimi <nazifarahimy6@gmail.com>
2025-05-19 09:50:40 +0430

    Style quote container with padding, centered layout, shadow, and rounded design

--- src/app/text/page.tsx
+++ src/app/text/page.tsx
@@ -9,11 +9,11 @@ export default function Text() {
                     <p className="text-xl font-normal px-3">هزار بار بیاید بهار، کافی نیست</p>
                     <p className="text-xl font-normal px-3">فاضل نظری</p>
                 </div>
-                 <div >
-                       <div ><FaQuoteRight /></div>
-                      <p >نهال بودم و در حسرتِ بهار! ولی</p>
-                      <p >درخت می‌شوم و شوق برگ و بارم نیست</p>
-                      <p>فاضل نظری</p>
+                 <div className="bg-gray-100 text-center w-[900px] h-auto  rounded-2xl border-box px-10 mx-auto mt-10 pb-5" >
+                       <div className="  mx-auto  w-[150px] text-center bg-gray-100 shadow-blue-100 shadow-md rounded-br-full rounded-bl-full flex justify-center pb-4 pt-3  mb-5"><FaQuoteRight /></div>
+                      <p className="text-xl font-normal px-3">نهال بودم و در حسرتِ بهار! ولی</p>
+                      <p className="text-xl font-normal px-3">درخت می‌شوم و شوق برگ و بارم نیست</p>
+                      <p className="text-xl font-normal px-3">فاضل نظری</p>
                   </div>
         </div>
   )
