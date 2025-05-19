9a5fb91b2f86809b3af92d06c5647d7d77dc150f
nazifaRahimi <nazifarahimy6@gmail.com>
2025-05-19 09:45:04 +0430

    Add romantic spring quote section with Persian poetry by Fazel Nazari

---
+++ src/app/text/page.tsx
@@ -0,0 +1,14 @@
+import { FaQuoteRight } from "react-icons/fa";
+export default function Home() {
+  return (
+          <div >
+                <h2 >متن عاشقانه فصل بهار</h2>
+                <div >
+                    <div ><FaQuoteRight /></div>
+                    <p >چنان که یخ زده تقویم‌ها اگر هر روز</p>
+                    <p >هزار بار بیاید بهار، کافی نیست</p>
+                    <p >فاضل نظری</p>
+                </div>
+        </div>
+  )
+}
