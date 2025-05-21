ac26b5718d06f80d091413f0fdeac99cbfb31117
nazifaRahimi <nazifarahimy6@gmail.com>
2025-05-21 20:23:42 +0430

    Add poetic spring quote structure and content

--- src/app/poeticsentence/page.tsx
+++ src/app/poeticsentence/page.tsx
@@ -4,6 +4,16 @@ const Poeticsentence = () => {
     return (  
         <div className="w-full  box-border px-[80px] py-10">
             <h1 className="text-2xl mt-10 mb-4 font-semibold  text-center">جملات ادبی در مورد بهار</h1>
+              <div>
+                <div><FaQuoteRight /></div>
+                <p >بـــهار را ببین</p>
+                <p >چگونه از خود می تــکانَد برگ های فرسوده را</p>
+                <p >تو هم جوانه ای بزن</p>
+                <p>ســبز شـــو</p>
+                <p >بـهـار</p>
+                <p>ادامه ی لبخندهای تو خواهد بود…</p>
+                <p >“مینا آقازاده”</p>
+            </div>
         </div>
     );
 }
