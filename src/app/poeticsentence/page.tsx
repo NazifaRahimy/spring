7356e6064db16e38f34a5538cf031b436d2dec6e
nazifaRahimi <nazifarahimy6@gmail.com>
2025-05-21 20:15:48 +0430

    Add <h1> tag for spring quotes title

---
+++ src/app/poeticsentence/page.tsx
@@ -0,0 +1,11 @@
+import { FaQuoteRight } from "react-icons/fa";
+
+const Poeticsentence = () => {
+    return (  
+        <div className="w-full  box-border px-[80px] py-10">
+            <h1>جملات ادبی در مورد بهار</h1>
+        </div>
+    );
+}
+ 
+export default Poeticsentence;
