e9859d454dab7789602a733e4943429d56bb4dd2
nazifaRahimi <nazifarahimy6@gmail.com>
2025-05-21 20:17:23 +0430

    Style <h1> title with Tailwind classes

--- src/app/poeticsentence/page.tsx
+++ src/app/poeticsentence/page.tsx
@@ -3,7 +3,7 @@ import { FaQuoteRight } from "react-icons/fa";
 const Poeticsentence = () => {
     return (  
         <div className="w-full  box-border px-[80px] py-10">
-            <h1>جملات ادبی در مورد بهار</h1>
+            <h1 className="text-2xl mt-10 mb-4 font-semibold  text-center">جملات ادبی در مورد بهار</h1>
         </div>
     );
 }
