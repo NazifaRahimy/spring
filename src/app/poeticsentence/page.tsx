04ee839bee14de788c0ac1f2bbc3f5f2c7b3e871
nazifaRahimi <nazifarahimy6@gmail.com>
2025-05-21 20:24:26 +0430

    Style spring quote section with Tailwind CSS classes

--- src/app/poeticsentence/page.tsx
+++ src/app/poeticsentence/page.tsx
@@ -4,15 +4,15 @@ const Poeticsentence = () => {
     return (  
         <div className="w-full  box-border px-[80px] py-10">
             <h1 className="text-2xl mt-10 mb-4 font-semibold  text-center">جملات ادبی در مورد بهار</h1>
-              <div>
-                <div><FaQuoteRight /></div>
-                <p >بـــهار را ببین</p>
-                <p >چگونه از خود می تــکانَد برگ های فرسوده را</p>
-                <p >تو هم جوانه ای بزن</p>
-                <p>ســبز شـــو</p>
-                <p >بـهـار</p>
-                <p>ادامه ی لبخندهای تو خواهد بود…</p>
-                <p >“مینا آقازاده”</p>
+              <div className="bg-gray-100 text-center w-[900px] h-auto   rounded-2xl border-box px-10 mx-auto mt-10 pb-5" >
+                <div className="  mx-auto  w-[150px] text-center bg-gray-100 shadow-blue-100 shadow-md rounded-br-full rounded-bl-full flex justify-center pb-4 pt-3  mb-5"><FaQuoteRight /></div>
+                <p className="text-xl font-normal px-3">بـــهار را ببین</p>
+                <p className="text-xl font-normal px-3">چگونه از خود می تــکانَد برگ های فرسوده را</p>
+                <p className="text-xl font-normal px-3">تو هم جوانه ای بزن</p>
+                <p className="text-xl font-normal px-3">ســبز شـــو</p>
+                <p className="text-xl font-normal px-3">بـهـار</p>
+                <p className="text-xl font-normal px-3">ادامه ی لبخندهای تو خواهد بود…</p>
+                <p className="text-xl font-normal px-3">“مینا آقازاده”</p>
             </div>
         </div>
     );
