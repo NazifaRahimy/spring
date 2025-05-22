20d9c045dd381b7ef45e14dd96f090c4ceb90487
nazifaRahimi <nazifarahimy6@gmail.com>
2025-05-22 11:16:58 +0430

    Style poetry component with centered layout, shadow, and rounded borders

--- src/app/poeticsentence/page.tsx
+++ src/app/poeticsentence/page.tsx
@@ -14,9 +14,9 @@ const Poeticsentence = () => {
                 <p className="text-xl font-normal px-3">ادامه ی لبخندهای تو خواهد بود…</p>
                 <p className="text-xl font-normal px-3">“مینا آقازاده”</p>
             </div>
-             <div >
-                <div ><FaQuoteRight /></div>
-                <h2 >وصفی نیست، برای شادی عمیقی که در قلب من است از دیدن تو بهار زندگیم
+             <div className="bg-gray-100 text-center w-[900px] h-auto  rounded-2xl border-box px-10 mt-10 mx-auto" >
+                <div className="  mx-auto  w-[150px] text-center bg-gray-100 shadow-blue-100 shadow-md rounded-br-full rounded-bl-full flex justify-center pb-4 pt-3  mb-5"><FaQuoteRight /></div>
+                <h2 className="text-xl font-normal mt-6  pb-10 px-20 text-center ">وصفی نیست، برای شادی عمیقی که در قلب من است از دیدن تو بهار زندگیم
                        دوستت دارم</h2>
             </div>
         </div>
