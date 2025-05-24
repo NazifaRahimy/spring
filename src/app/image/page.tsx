8f277b8c0e248b96ca6f6ed8ac31100fe0b333cc
nazifaRahimi <nazifarahimy6@gmail.com>
2025-05-24 22:31:48 +0430

    Style section and heading using Tailwind CSS

--- src/app/image/page.tsx
+++ src/app/image/page.tsx
@@ -1,7 +1,7 @@
 const Image = () => {
     return (  
-        <div >
-           <h2 >عکس نوشته عاشقانه بهاری</h2>
+        <div className="w-full  box-border px-[80px] py-10">
+           <h2 className="mt-10 text-center mb-4 text-[#232323] font-semibold text-xl">عکس نوشته عاشقانه بهاری</h2>
         </div>
     );
 }
