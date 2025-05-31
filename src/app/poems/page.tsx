f3e3830e5669296b99319ff27b43cb1869e50da2
nazifaRahimi <nazifarahimy6@gmail.com>
2025-05-31 21:35:02 +0430

    Style poetry section with padding and centered title

--- src/app/poems/page.tsx
+++ src/app/poems/page.tsx
@@ -1,7 +1,7 @@
 const Poems = () => {
      return ( 
-        <div >
-            <h2 >اشعار عاشقانه درباره بهار</h2>
+        <div className="w-full  box-border px-[80px] py-10">
+            <h2 className="mt-10 text-center mb-4 text-[#232323] font-semibold text-xl">اشعار عاشقانه درباره بهار</h2>
         </div>
       );
 }
